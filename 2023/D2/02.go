e8b75b6a829d05eee70f66b9b897b4fe738308e8
nijuyonkadesu <24ksriramv@gmail.com>
2023-12-03 18:24:12 +0530

    02 p2

--- 2023/D2/02.go
+++ 2023/D2/02.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 )
 
@@ -21,7 +22,19 @@ type Game struct {
 }
 
 func main() {
-    fmt.Println(dontLieJoshua())
+    // fmt.Println(dontLieJoshua())
+    fmt.Println(JoshuaCopeHard())
+}
+
+func (g *Game) maxCubes() Game {
+    game := Game{ id: 69 }
+    game.red = append(game.red, slices.Max(g.red))
+    game.green= append(game.green, slices.Max(g.green))
+    game.blue= append(game.blue, slices.Max(g.blue))
+    if DEBUG {
+        fmt.Print("max: ", game)
+    }
+    return game
 }
 
 func (g *Game) isPossible() bool {
@@ -55,6 +68,34 @@ func (g *Game) addCubes(count int, color string) {
     }
 }
 
+// part 2
+func JoshuaCopeHard() int {
+    cubeMulTot := 0
+    file, err := os.Open("02.txt")
+    if err != nil {
+        fmt.Println(err)
+        return 0
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        claim := scanner.Text()
+        if claim == "" {
+            continue
+        }
+        game, err := toGame(claim)
+        if err != nil {
+            fmt.Print(err)
+        }
+        maxCubeGame := game.maxCubes()
+        cubesMul := maxCubeGame.red[0] * maxCubeGame.green[0] * maxCubeGame.blue[0]
+        cubeMulTot = cubeMulTot + cubesMul
+    }
+    return cubeMulTot 
+}
+
+// part 1
 func dontLieJoshua() int {
     sumOfGameIds := 0
     file, err := os.Open("02.txt")
